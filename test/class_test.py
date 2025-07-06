936fd74327d3018fcbd82f67e5dfa260d0140283
harmin-parra <harmin.parra@gmail.com>
2025-07-06 12:28:15 +0200

    improve import statements

--- test/class_test.py
+++ test/class_test.py
@@ -1,4 +1,4 @@
-from src.sample_class import *
+from src.sample_class import SampleClass
 
 
 class TestClass:
--- test/module_test.py
+++ test/module_test.py
@@ -1,23 +1,23 @@
-from src.sample_module import *
+from src import sample_module
 
 
 def test_1_no_args():
-    function_no_args()
+    sample_module.function_no_args()
 
 def test_2_args():
-    function_args(1, 2)
+    sample_module.function_args(1, 2)
 
 def test_3_call():
-    function_calling_method()
+    sample_module.function_calling_method()
 
 def test_4_fail():
-    function_failure()
+    sample_module.function_failure()
 
 def test_5_kwargs():
-    function_kwargs(param2=4, param1=3)
+    sample_module.function_kwargs(param2=4, param1=3)
 
 def test_6_kwargs():
-    function_kwargs(param2=4)
+    sample_module.function_kwargs(param2=4)
 
 def test_7_args():
-    function_args_kwargs(5, param3=6)
+    sample_module.function_args_kwargs(5, param3=6)
